de2bbc3a8db40a6c1adfab53493fd12e34578501
Joao Paulo Reis Marques <joaopaulo.rm7@gmail.com>
2022-12-06 20:30:07 -0400

    add page objects

---
+++ PageObjects/BudgetTestCases.py
@@ -0,0 +1,90 @@
+import unittest
+import warnings
+from datetime import date
+
+from MainPage import MainPage
+from BudgetPage import BudgetPage
+from AddBudgetPage import AddBudgetPage
+from appium import webdriver
+from Data import TestData
+import time
+
+from PageObjects.IntroPage import IntroPage
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['deviceName'] = 'emulator-5556'
+        desired_caps['appPackage'] = 'protect.budgetwatch'
+        desired_caps['appActivity'] = '.MainActivity'
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=DeprecationWarning)
+            self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+    def test_create_new_budget(self):
+        self.driver.implicitly_wait(30)
+        intro_page = IntroPage(self.driver)
+        intro_page.click_skip()
+        self.driver.implicitly_wait(30)
+        main_page = MainPage(self.driver)
+        main_page.click_budget()
+        budget_page = BudgetPage(self.driver)
+        budget_page.click_add()
+        add_page = AddBudgetPage(self.driver)
+        self.driver.implicitly_wait(30)
+        add_page.type_budget_type(TestData.budget_type)
+        add_page.type_budget_value(TestData.budget_value)
+        add_page.click_save_button()
+        budget_page = BudgetPage(self.driver)
+        self.assertEqual(budget_page.get_first_budget(), TestData.budget_type)
+
+
+    def test_app_budget_add_name_invalid(self):
+        self.driver.implicitly_wait(30)
+        intro_page = IntroPage(self.driver)
+        intro_page.click_skip()
+        # clicar em budget
+        main_page = MainPage(self.driver)
+        main_page.click_budget()
+        budget_page = BudgetPage(self.driver)
+        budget_page.click_add()
+        #Adicionar nome
+        add_page = AddBudgetPage(self.driver)
+        self.driver.implicitly_wait(30)
+        # Adicionar valor
+        time.sleep(5)
+        add_page.type_budget_value(TestData.budget_value)
+        #Save
+        add_page.click_save_button()
+        #Validação
+        self.assertEqual(add_page.get_error(), TestData.msg_budget_type_empty)
+
+    def test_app_budget_add_value_invalid(self):
+        self.driver.implicitly_wait(30)
+        intro_page = IntroPage(self.driver)
+        intro_page.click_skip()
+        # clicar em budget
+        main_page = MainPage(self.driver)
+        main_page.click_budget()
+        budget_page = BudgetPage(self.driver)
+        budget_page.click_add()
+        #Adicionar nome
+        add_page = AddBudgetPage(self.driver)
+        self.driver.implicitly_wait(30)
+        add_page.type_budget_type(TestData.budget_type)
+        # Adicionar valor
+        self.driver.implicitly_wait(20)
+        time.sleep(5)
+        add_page.type_budget_value(TestData.budget_value_invalid)
+        #Save
+        add_page.click_save_button()
+        #Validação
+        self.assertEqual(add_page.get_error(), TestData.msg_budget_value_empty)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- budget_test.py
+++ budget_test.py
@@ -50,6 +50,33 @@ class AndroidBudget(unittest.TestCase):
 
         self.assertEqual("energia", self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'energia')]").get_attribute('text'))
 
+    def test_app_budget_add_nome_invalido(self):
+
+        self.driver.implicitly_wait(30)
+
+        if self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Welcome to Budget Watch')]"):
+            skip = self.driver.find_element(By.ID, 'protect.budgetwatch:id/skip')
+            skip.click()
+
+        # clicar em budget
+        budget = self.driver.find_element(By.ID, 'protect.budgetwatch:id/image')
+        budget.click()
+
+        add = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_add')
+        add.click()
+
+        name = self.driver.find_element(By.ID, 'protect.budgetwatch:id/budgetNameEdit')
+        name.set_text("")
+
+        value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
+        value.set_text("100")
+
+        save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
+        save.click()
+
+        self.assertEqual('Budget type is empty',
+                         self.driver.find_element(By.ID, 'protect.budgetwatch:id/snackbar_text').get_attribute('text'))
+
     def test_app_budget_add_valor_invalido(self):
 
         self.driver.implicitly_wait(30)
@@ -66,7 +93,7 @@ class AndroidBudget(unittest.TestCase):
         add.click()
 
         name = self.driver.find_element(By.ID, 'protect.budgetwatch:id/budgetNameEdit')
-        name.set_text("test123")
+        name.set_text("test")
 
         value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
         value.set_text("abcdefg")
@@ -122,10 +149,10 @@ class AndroidBudget(unittest.TestCase):
         save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
         save.click()
 
-        self.assertEqual('Budget value is empty',
+        self.assertEqual('Budget type is empty',
                          self.driver.find_element(By.ID, 'protect.budgetwatch:id/snackbar_text').get_attribute('text'))
 
-    def test_app_budget_add_valor_acima_30(self):
+    def test_app_budget_add_valor_acima_10(self):
 
         self.driver.implicitly_wait(30)
 
@@ -144,13 +171,47 @@ class AndroidBudget(unittest.TestCase):
         name.set_text("Gasto 1")
 
         value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
-        value.set_text("300000000000000000000000000000000000000000")
+        value.set_text("12345678910")
 
         save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
         save.click()
 
         self.assertEqual('Budget value is empty',
                          self.driver.find_element(By.ID, 'protect.budgetwatch:id/snackbar_text').get_attribute('text'))
+
+    def test_app_budget_edit(self):
+
+        self.driver.implicitly_wait(30)
+
+        actions = TouchAction(self.driver)
+
+        if self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Welcome to Budget Watch')]"):
+            skip = self.driver.find_element(By.ID, 'protect.budgetwatch:id/skip')
+            skip.click()
+
+        # clicar em budget
+        budget = self.driver.find_element(By.XPATH,
+                                          "//android.widget.TextView[contains(@text, 'Budgets')]")
+        budget.click()
+
+        add = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_add')
+        add.click()
+
+        name = self.driver.find_element(By.ID, 'protect.budgetwatch:id/budgetNameEdit')
+        name.set_text("energia")
+
+        value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
+        value.set_text("500")
+
+        save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
+        save.click()
+
+        # Long click no budget salvo
+        budgetEdit = self.driver.find_element(By.XPATH,
+                                          "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.RelativeLayout/android.widget.ListView/android.widget.LinearLayout/android.widget.LinearLayout")
+
+        actions.long_press(budgetEdit)
+        actions.perform()
 
     def tearDown(self):
         self.driver.quit()
